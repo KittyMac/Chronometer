ae114d3884a7cad325ec3b40a043da844702ca94
Rocco Bowling <rocco@chimerasw.com>
2025-07-10 21:19:07 -0400

    updated swift tools

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "Chronometer",
     platforms: [
-        .macOS(.v10_13), .iOS(.v11)
+        .macOS(.v12),
+        .iOS(.v11)
     ],
     products: [
         .library(name: "Chronometer", targets: ["Chronometer"]),
